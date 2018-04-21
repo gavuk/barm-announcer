84263ec3b9406b5b1bffeee9a4859e2db5e5ea56
Markus <markus.iversen@gmail.com>
2018-04-21 15:58:53 +0000

    Changed data to JSON array

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var os = require('os');
 
 // Create the HTTP server
 http.createServer(function (req, res) {
-  res.write(os.hostname()); //write out the hostname
+  // Create a JSON string
+  var outputArray = [os.hostname(), 0];
+  var jsonOutput = JSON.stringify(outputArray);
+
+  res.write(jsonOutput); //write out the JSON string
   res.end(); //end the response
 }).listen(5001); //the server object
