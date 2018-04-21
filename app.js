a8ec6a96a1d33250afec5cf8ec406bc8fcd4569d
Gavin Abson <gavin.abson@gmail.com>
2018-04-21 18:19:47 +0100

    Updated announcer to add a port number for either the wifi config page or drinks interface

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 // Requirements
 var http = require('http');
 var os = require('os');
+var ps = require('ps-node');
+
+// Function to check if the wifi config tool is running
+function checkProc(proc, callback) {
+  ps.lookup({
+    command: proc,
+    psargs: 'ux'
+    }, function(err, resultList) {
+    if (err) {
+        throw new Error( err );
+    }
+
+    // Return the number of processes
+    return callback(resultList.length);
+  });
+}
 
 // Create the HTTP server
 http.createServer(function (req, res) {
-  // Create a JSON string
-  var outputArray = [os.hostname(), 0];
-  var jsonOutput = JSON.stringify(outputArray);
+  // Check if the wifi config tool is currently running
+  checkProc('/usr/local/n/versions/node/6.10.2/bin/node server.js', function(response) {
+    // Set the port based on the number or processes found
+    if (response > 0) {
+      var port = 80;
+    } else {
+      var port = 5001;
+    }
 
-  res.write(jsonOutput); //write out the JSON string
-  res.end(); //end the response
+    // Create a JSON string
+    var outputArray = [os.hostname(), port];
+    var jsonOutput = JSON.stringify(outputArray);
+    res.write(jsonOutput); //write out the JSON string
+    res.end(); //end the response
+  });
+
 }).listen(5001); //the server object
